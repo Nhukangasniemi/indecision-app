668b364b38fb4d39dfa5e0ec00acf8ae9d25ef52
Nhukangasniemi <quynhnhu.ho2310@gmail.com>
2019-02-26 08:10:24 +0200

    Styling all buttons

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Option from './Option';
 
-const Options = (props) => {
-  return (
+const Options = (props) => (
     <div>
-      <button onClick={props.handleDeleteOptions}>Remove All</button>
+    <div className='widget-header'>
+    <h3>Your Options</h3>
+    <button 
+      onClick={props.handleDeleteOptions}
+      className='button button--link'
+    >Remove All</button>
+    </div>
+    
       {props.options.length === 0 && <p>Please add an option to get started!</p>}
       {
         props.options.map((option) => (
@@ -17,6 +23,5 @@ const Options = (props) => {
       }
     </div>
   );
-};
 
 export default Options;
--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,20 @@ module.exports = {
   },
   module: {
     rules: [{
-      loader: 'babel-loader',
+      loader: 'babel-loader', //teach webpack how to use babel
       test: /\.js$/,
       exclude: /node_modules/
+    }, {
+      test: /\.s?css$/, //css-loader take in css and converted it to JS representation
+      use: [
+        'style-loader', //inject it to the DOM under <style> tag
+        'css-loader',
+        'sass-loader'
+      ]
     }]
+  }, 
+  devtool: 'cheap-module-eval-source-map', //help to debug and show bugs in source code
+  devServer: { //same like live-server but have webpack feature
+    contentBase: path.join(__dirname, 'public') 
   }
 };
