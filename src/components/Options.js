1535ff481e66e05443604d15c1c1d67f300c8b8a
Nhukangasniemi <quynhnhu.ho2310@gmail.com>
2019-02-27 12:34:24 +0200

    Styling modal

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,19 +4,20 @@ import Option from './Option';
 const Options = (props) => (
     <div>
     <div className='widget-header'>
-    <h3>Your Options</h3>
+    <h3 className='widget-header__title'>Your Options</h3>
     <button 
       onClick={props.handleDeleteOptions}
       className='button button--link'
     >Remove All</button>
     </div>
     
-      {props.options.length === 0 && <p>Please add an option to get started!</p>}
+      {props.options.length === 0 && <p className='widget__message'>Please add an option to get started!</p>}
       {
-        props.options.map((option) => (
+        props.options.map((option, index) => (
           <Option
             key={option}
             optionText={option}
+            count={index+1}
             handleDeleteOption={props.handleDeleteOption}
           />
         ))
